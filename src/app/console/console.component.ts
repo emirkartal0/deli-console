f4b22303ca541aed0002ac9c25394b63f3c36a1a
emir <emirkartal00@gmail.com>
2022-12-02 09:06:38 +0300

    finish

--- src/app/console/console.component.ts
+++ src/app/console/console.component.ts
@@ -7,17 +7,17 @@ import { AfterViewInit, Component } from '@angular/core';
 })
 
 export class ConsoleComponent implements AfterViewInit {
-  binding: string = '';
+
+  terminalInput: string = '';
   userInput: any = '';
   terminalOutput: any = '';
 
-  kosul = 'evet'
   constructor() { }
 
   ngAfterViewInit(): void {
     const _onBackspacePress = this.backspace.bind(this)
-    document.addEventListener("keydown", _onBackspacePress);
     const _onKeyPress = this.key.bind(this);
+    document.addEventListener("keydown", _onBackspacePress);
     document.addEventListener("keypress", _onKeyPress);
     this.userInput = document.getElementById("userInput");
     this.terminalOutput = document.getElementById("terminalOutput");
@@ -27,12 +27,10 @@ export class ConsoleComponent implements AfterViewInit {
     if (e.key !== 'Backspace' && e.key !== 'Delete') {
       return;
     }
-    console.log(this.userInput.innerHTML)
     this.userInput.innerHTML = this.userInput.innerHTML.slice(0, this.userInput.innerHTML.length - 1);
   }
 
   key(e: KeyboardEvent) {
-
     this.userInput = document.getElementById("userInput");
     const input = this.userInput.innerHTML;
     if (e.key === "Enter") {
@@ -47,21 +45,18 @@ export class ConsoleComponent implements AfterViewInit {
     let output
     input = input.toLowerCase();
 
-    output = `<div class="terminal-line "><span class="success">➜</span> <span class="directory">~</span> ${input}</div>`;
+    output = `<div><span>➜</span> <span class="directory">~</span> ${input}</div>`;
     if (!this.COMMANDS.hasOwnProperty(input)) {
-      output += `<div class="terminal-line text-red-500 ">no such command: <span class="output">"${input}"</span></div>`;
+      output += `<div class="text-red-500 ">no such command: <span class="output">"${input}"</span></div>`;
     } else {
       output += `<div class="output text-[#2afc14]"> ${this.COMMANDS[input]} </div>`;
     }
-
-    this.terminalOutput.innerHTML = `${this.terminalOutput.innerHTML
-      }<div class="terminal-line text-stone-300">${output}</div>`;
-    this.terminalOutput.scrollTop = this.terminalOutput.scrollHeight;
-    console.log(this.terminalOutput.scrollHeight)
+    this.terminalOutput.innerHTML = `${this.terminalOutput.innerHTML}<div class=" text-stone-300">${output}</div>`;
+    //this.terminalOutput.scrollTop = this.terminalOutput.scrollHeight;
   }
 
   COMMANDS: any = {
-    help: 'Supported commands: ["<span class="text-blue-500">about</span>", "<span class="text-blue-500">experience</span>", "<span class="text-blue-500">education</span>", "<span class="text-blue-500">skills</span>", "<span class="text-blue-500">contact</span>"]',
+    help: 'Supported commands: ( "<span class="text-blue-500">about</span>", "<span class="text-blue-500">experience</span>", "<span class="text-blue-500">education</span>", "<span class="text-blue-500">skills</span>", "<span class="text-blue-500">contact</span>" )',
     about:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     skills:
@@ -72,9 +67,8 @@ export class ConsoleComponent implements AfterViewInit {
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     contact:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    deli: "<span class='text-blue-500' style='font-size: 5rem;'>✈️</span>",
-    party: "🎉🎉🎉",
-    beer: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    deli: "<span class='text-cyan-300' style='font-size: 5rem;'>✈️</span>",
+    party: "🎉🎉🎉"
   };
 
 }
